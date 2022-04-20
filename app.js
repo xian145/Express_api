88b6b8dd1c0895821346f2db20d1240605a64597
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2022-04-20 12:23:20 -0700

    se agrego metods http donde se agrego objetos y los objetos a una lista

---
+++ app.js
@@ -0,0 +1,27 @@
+//usando objeto express
+const express = require('express')
+
+//app de Express
+const app = express()
+
+//indicamos que usaremos json (para un api)
+app.use(express.json())
+
+//puerto en que vamos a ver nuestra app
+const port = 3000
+
+//http methods
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api explorers GET ALL request ${new Date()}`);
+    const explorer1 = {id: 1, name: "Abraham"}
+    const explorer2 = {id: 2, name: "Ariel"}
+    const explorer3 = {id: 3, name: "Heichi"}
+    const explorer4 = {id: 4, name: "Horacio"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+})
+
+// Con esto inicializamos esta app (esto debe estar a lo ultimo?)
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
